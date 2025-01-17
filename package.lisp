1418ea9d6e7eea163b83b1de7b8c42fce723dc73
Eugene Zaikonnikov <ez@norphonic.com>
2025-01-17 15:06:36 +0100

    add exports

--- package.lisp
+++ package.lisp
@@ -1,4 +1,9 @@
 ;;;; package.lisp
 
 (defpackage #:menuett
-  (:use #:cl #:cl-evdev))
+  (:use #:cl #:cl-evdev)
+  (:export #:display #:write-lcd #:swipe-left #:swipe-right #:buzz #:initialize-display
+	   #:menu-interaction #:writeout-screen #:dummy-menu-item #:make-menu-text
+	   #:+lcd-cls+ #:+lcd-blink-off+ #:+lcd-blink-on+ #:+lcd-cursor-off+ #:+lcd-cursor-on+
+	   #:+lcd-cursor-home+ #:+lcd-cursor-left+ #:+lcd-cursor-right+ #:+lcd-scroll-left+
+	   #:+lcd-scroll-right+ #:+lcd-init+ #:+lcd-kill+))
